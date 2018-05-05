8b94faf270d55e58a8f3f893d80c7d3a1af483ff
Robert <robert.nystrom@husqvarnagroup.com>
2018-05-05 22:50:57 +0200

    added activity and changing between activities

--- app/src/main/java/com/example/hackaton/hackaton/StartProfileActivity.java
+++ app/src/main/java/com/example/hackaton/hackaton/StartProfileActivity.java
@@ -1,11 +1,13 @@
 package com.example.hackaton.hackaton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class StartProfileActivity extends AppCompatActivity {
 
@@ -24,6 +26,14 @@ public class StartProfileActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 
 }
